592734dc8a41bb9313371bd7c6963ee321455eeb
RafaelMatos <rafael-matos@outlook.com>
2024-02-26 12:48:20 -0300

    feat: add bookings page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
         <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">
           Agendamentos
         </h2>
-        <BookingItem />
+        {/* <BookingItem booking={ db.} /> */}
       </div>
 
       <div className=" mt-6">
