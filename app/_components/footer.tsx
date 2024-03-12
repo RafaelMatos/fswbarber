262e1ec4dcd0f113be80dad3cf755cc610f80a6c
RafaelMatos <rafael-matos@outlook.com>
2024-03-12 18:28:18 -0300

    feat: add link to dev github on footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link'
+
 const Footer = () => {
   return (
     <footer>
-      <div className="w-full bg-secondary py-6 px-5">
+      <div className="w-full flex justify-between bg-secondary py-6 px-5">
+        <p className="text-gray-400 text-xs opacity-75">
+          Developed by
+          <span className="font-bold">
+            <Link href={process.env.NEXT_PUBLIC_GITHUB!}> @RafaelMatos</Link>
+          </span>
+        </p>
         <p className="text-gray-400 text-xs opacity-75">
           © 2023 Copyright <span className="font-bold">FSW Barber</span>
         </p>
